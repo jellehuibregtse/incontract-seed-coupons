86decf492ba6e8af7b147af4d05ef36f050c061f
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2021-12-02 09:41:28 +0100

    feat: add coupon types

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ def seed_coupons(amount_of_coupons: int = 100,
                  discount: int = 100,
                  amount_of_uses: int = 1,
                  issuer_id: int = 'null',
+                 coupon_type: str = 'contract',
                  prod: bool = False) -> None:
     """
     Seed the coupons table.
@@ -55,13 +56,14 @@ def seed_coupons(amount_of_coupons: int = 100,
     :param discount: Discount percentage.
     :param amount_of_uses: The amount of times the coupon can be used.
     :param issuer_id: The issuer id.
+    :param coupon_type: The type of coupon (contract, subscription or all).
     :param prod: If True, seeds the prod database.
     """
     # If production run the proxy and use the production database.
     if prod:
         run_proxy()
 
-    db = connect_database(host="localhost",
+    db = connect_database(host="0.0.0.0" if prod else "localhost",
                           port=1234 if prod else 3306,
                           user=os.environ['PROD_USER'] if prod else os.environ['LOCAL_USER'],
                           password=os.environ['PROD_PASSWORD'] if prod else os.environ['LOCAL_PASSWORD'],
@@ -72,7 +74,7 @@ def seed_coupons(amount_of_coupons: int = 100,
     expired_on = str(datetime.datetime.today() + datetime.timedelta(days=expires_in))
 
     # Headers for the CSV file.
-    header = ['CODE', 'DATE_CREATED', 'EXPIRY_DATE', 'DISCOUNT', 'AMOUNT_OF_USES', 'ISSUER_ID']
+    header = ['CODE', 'DATE_CREATED', 'EXPIRY_DATE', 'DISCOUNT', 'AMOUNT_OF_USES', 'ISSUER_ID', 'TYPE']
     rows = []
 
     for i in range(amount_of_coupons):
@@ -83,11 +85,11 @@ def seed_coupons(amount_of_coupons: int = 100,
             if not cur.fetchone():
                 break
 
-        row = [code, now, expired_on, discount, amount_of_uses, issuer_id]
+        row = [code, now, expired_on, discount, amount_of_uses, issuer_id, coupon_type]
         rows.append(row)
 
         cur.execute(f"INSERT INTO coupon VALUES (default, null, '{code}', '{now}', {discount}, null, "
-                    f"'{expired_on}', {amount_of_uses}, {issuer_id})")
+                    f"'{expired_on}', {amount_of_uses}, {issuer_id}, '{coupon_type}')")
 
     db.commit()
 
@@ -107,8 +109,7 @@ def seed_coupons(amount_of_coupons: int = 100,
 
 
 if __name__ == '__main__':
-    seed_coupons(amount_of_coupons=25, amount_of_uses=5)
-    seed_coupons(amount_of_coupons=25, amount_of_uses=1)
-    seed_coupons(amount_of_coupons=25, amount_of_uses=5, discount=50)
-    seed_coupons(amount_of_coupons=25, amount_of_uses=1, discount=50)
-
+    seed_coupons(amount_of_coupons=25, amount_of_uses=5, issuer_id=806)
+    seed_coupons(amount_of_coupons=25, amount_of_uses=1, issuer_id=806)
+    seed_coupons(amount_of_coupons=25, amount_of_uses=5, discount=50, issuer_id=806)
+    seed_coupons(amount_of_coupons=25, amount_of_uses=1, discount=50, issuer_id=806)
